d788b073e7d1edfa77d5e5cf5ebeac5a2503c86c
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-14 16:31:40 +0530

    Add implementation for finding the longest subarray with sum K.

---
+++ Arrays/Medium/longestSubArrayWithSumK.cpp
@@ -0,0 +1,16 @@
+// Longest subarray with sum K.
+// Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
+// Example : 
+
+// Input: nums = [10, 5, 2, 7, 1, 9],  k=15
+// Output: 4
+// Explanation:
+// The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4.This sub-array starts at index 1 and ends at index 4, and the sum of its elements (5 + 2 + 7 + 1) equals 15.Therefore,the length of this sub-array is 4.
+
+class Solution{
+public:
+    int longestSubarray(vector<int> &nums, int k){
+        
+    }
+};
