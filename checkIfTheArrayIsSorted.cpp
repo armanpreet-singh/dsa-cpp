8b18dcc38ed8ff20250be88bc5bf120cca7b24bb
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-14 12:03:20 +0530

    Add implementation for checking if an array is sorted.

---
+++ checkIfTheArrayIsSorted.cpp
@@ -0,0 +1,23 @@
+// Check if the Array is Sorted.
+//Given an array nums of n integers, return true if the array nums is sorted in non-decreasing order or else false.
+
+// Example 1
+
+// Input : nums = [1, 2, 3, 4, 5]
+
+// Output : true
+
+// Explanation : For all i (1 <= i <= 4) it holds nums[i] <= nums[i+1], hence it is sorted and we return true.
+
+class Solution {
+public:
+    bool isSorted(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
