dfed0633c3253664dd9d4ab30a95683bf5fac32a
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-14 11:58:11 +0530

    Add implementation for finding the largest element in an array.

---
+++ largestElement.cpp
@@ -0,0 +1,23 @@
+// Largest Element
+// Given an array of integers nums, return the value of the largest element in the array.
+
+// Example 1
+
+// Input: nums = [3, 3, 6, 1]
+
+// Output: 6
+
+// Explanation: The largest element in array is 6
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+int largest = nums[0];
+for(int i=1; i<nums.size(); i++){
+    if(nums[i]>largest){
+        largest = nums[i];
+    }
+}
+return largest;
+    }
+};
