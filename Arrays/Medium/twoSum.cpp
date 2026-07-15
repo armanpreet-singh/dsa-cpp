844aadbc5b0a2148098a122b0f5cdf8f7c28699e
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-15 15:34:08 +0530

    Add optimized implementation for Two Sum problem using hash map.

--- Arrays/Medium/twoSum.cpp
+++ Arrays/Medium/twoSum.cpp
@@ -24,3 +24,22 @@ public:
         return {};
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;                                    // Using Hash Map.
+
+        for(int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if(mp.find(need) != mp.end()) {
+                return {mp[need], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
