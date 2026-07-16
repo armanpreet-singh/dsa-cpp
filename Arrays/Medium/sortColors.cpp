6c75fdeef371be48f782d603d8d3a59b6835d006
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-16 13:51:46 +0530

    Add 3-Counter Approach implementation for sorting colors.

--- Arrays/Medium/sortColors.cpp
+++ Arrays/Medium/sortColors.cpp
@@ -42,3 +42,32 @@ public:
         }   
     }
 };
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, one = 0, two = 0;                  // The 3-Counter Approach.
+
+        // Count frequencies
+        for (int x : nums) {
+            if (x == 0)
+                zero++;
+            else if (x == 1)
+                one++;
+            else
+                two++;
+        }
+
+        // Fill the array
+        int i = 0;
+
+        while (zero--)
+            nums[i++] = 0;
+
+        while (one--)
+            nums[i++] = 1;
+
+        while (two--)
+            nums[i++] = 2;
+    }
+};
