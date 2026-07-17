0463a849d9ee48cd8658294d49eaea5e0ad0a3d8
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-17 12:05:48 +0530

    Add verification step to majorityElement function for non-guaranteed majority cases

--- Arrays/Medium/majorityElement.cpp
+++ Arrays/Medium/majorityElement.cpp
@@ -58,3 +58,26 @@ public:
         return candidate;
     }
 };
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;                                // If Problem Doesn't Guarantee A Majority Element.
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0)
+                candidate = num;
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        // Verification
+        count = 0;
+        for (int num : nums)
+            if (num == candidate)
+                count++;
+
+        return (count > nums.size() / 2) ? candidate : -1;
+    }
+};
