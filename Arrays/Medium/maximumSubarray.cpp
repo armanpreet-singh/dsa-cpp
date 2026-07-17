b57c74b38c6432eb4391b11e2425a158f8dc947f
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-17 14:44:18 +0530

    Refactor maxSubArray function to implement a more efficient approach for finding the maximum subarray sum

--- Arrays/Medium/maximumSubarray.cpp
+++ Arrays/Medium/maximumSubarray.cpp
@@ -11,6 +11,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        int n = size(nums), ans = INT_MIN;                      // trying out every possible sub-array.
+        for(int i = 0; i < n; i++) 
+            for(int j = i, curSum = 0; j < n ; j++) 
+                curSum += nums[j],
+                ans = max(ans, curSum);        
+        return ans;
     }
 };
