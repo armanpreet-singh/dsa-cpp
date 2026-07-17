77d0271e45011c4d66197c2e7e041449ae9c69d8
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-17 14:45:46 +0530

    Refactor maxSubArray function to implement Kadane's Algorithm for improved efficiency

--- Arrays/Medium/maximumSubarray.cpp
+++ Arrays/Medium/maximumSubarray.cpp
@@ -19,3 +19,22 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){                       // Kadane's Algorithm.
+            sum+=nums[i];
+            if(sum>max){
+                max=sum;
+            }
+
+            if(sum<0){
+                sum=0;
+            }
+        }
+return max;
+    }
+};
