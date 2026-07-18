06bfdbad2252b221ce76f2de49f3793180228783
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-18 10:28:18 +0530

    Add new maxProfit implementation using a conditional approach for stock trading

--- Arrays/Medium/bestTimeToBuyAndSellStock.cpp
+++ Arrays/Medium/bestTimeToBuyAndSellStock.cpp
@@ -44,3 +44,29 @@ public:
         return profit;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size()==0)
+        return 0;
+
+        int minprice=prices[0];
+        int maxprofit=0;
+        for(int i=1;i<prices.size();i++)                      // Usinf If Else.
+        {
+            if(prices[i]<minprice)
+            {
+                minprice=prices[i];
+            }
+            else{
+                int profit=prices[i]-minprice;
+                if(profit>maxprofit){
+                  maxprofit=profit;
+            }
+            }
+            
+        }
+        return maxprofit;
+    }
+};
